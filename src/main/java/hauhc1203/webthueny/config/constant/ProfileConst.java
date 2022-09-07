74de727239b337370b4457d93adc2b91290a4941
hauhc1203 <hau.hc1203@gmail.com>
2022-09-07 21:55:54 +0700

    update model, them feedback

--- src/main/java/hauhc1203/webthueny/config/constant/ProfileConst.java
+++ src/main/java/hauhc1203/webthueny/config/constant/ProfileConst.java
@@ -1,7 +1,6 @@
 package hauhc1203.webthueny.config.constant;
 
 public class ProfileConst {
-//    public final static int BUSY=0;
     public final static int INIT_STATE=5;
     public final static int REGISTER_PROVIDE_SERVICE_SUCCESS=3;
     public final static int REGISTER_PROVIDE_SERVICE_FAILED=4;
