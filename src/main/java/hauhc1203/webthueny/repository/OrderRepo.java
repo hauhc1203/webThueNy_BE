e8c670e652267dce22d513f0aad0e8b031eaf453
duong2512 <tungduong25122002@gmail.com>
2022-09-08 17:25:09 +0700

    up show order

--- src/main/java/hauhc1203/webthueny/repository/OrderRepo.java
+++ src/main/java/hauhc1203/webthueny/repository/OrderRepo.java
@@ -1,7 +1,9 @@
 package hauhc1203.webthueny.repository;
 
 import hauhc1203.webthueny.models.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface OrderRepo extends CrudRepository<Order,Long> {
+public interface OrderRepo extends PagingAndSortingRepository<Order,Long> {
 }
--- src/main/java/hauhc1203/webthueny/services/OrderService.java
+++ src/main/java/hauhc1203/webthueny/services/OrderService.java
@@ -3,9 +3,13 @@ package hauhc1203.webthueny.services;
 import hauhc1203.webthueny.models.Order;
 import hauhc1203.webthueny.repository.OrderRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class OrderService {
     @Autowired
     OrderRepo orderRepo;
@@ -13,4 +17,6 @@ public class OrderService {
     public List<Order> getAll(){
         return (List<Order>) orderRepo.findAll();
     }
+
+
 }
