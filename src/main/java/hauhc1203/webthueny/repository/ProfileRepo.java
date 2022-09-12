20e2beb790588a81ac65dd2c82887d7d377c9129
quan <trandinhquan0711@gmail.com>
2022-09-12 13:45:38 +0700

    up

--- src/main/java/hauhc1203/webthueny/repository/ProfileRepo.java
+++ src/main/java/hauhc1203/webthueny/repository/ProfileRepo.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 public interface ProfileRepo extends PagingAndSortingRepository<Profile,Long> {
     Profile findByAppUserId(long id);
-    @Query(nativeQuery = true,value = "SELECT * FROM profile order by views desc limit 3")
+    @Query(nativeQuery = true,value = "SELECT * FROM profile order by views desc limit 6")
     List<Profile>getProfileByViews();
 
 }
